206ac7effc538d4623c4c048274f3aa9778ba2cf
raongang <sayllclubs@naver.com>
2020-03-26 18:31:31 +0900

    2020-03-26 raongang @Autowired add

--- tobyspringVol2_Ch1/src/main/java/springbook/learningtest/spring/ioc/SimpleAtBeanTest.java
+++ tobyspringVol2_Ch1/src/main/java/springbook/learningtest/spring/ioc/SimpleAtBeanTest.java
@@ -29,22 +29,31 @@ public class SimpleAtBeanTest {
 		 */
 		AbstractApplicationContext ac = new AnnotationConfigApplicationContext(BeanA.class,BeanB.class);
 			
-		BeanA beanA = ac.getBean(BeanA.class);
+		BeanA beanA = ac.getBean(BeanA.class); //type
 		assertThat(beanA, is(notNullValue()));
 		
 		BeanB beanB = ac.getBean(BeanB.class);
 		assertThat(beanB,is(notNullValue()));
 		
 		//@Autowired 설정 있어야 함.
+		
+		
 		assertThat(beanA.beanB, is(notNullValue()));
 
 	}//test
-	
 	/*
 		bean으로 등록할 애노테이션 설정을 가진 클래스를 static 클래스로 정의한다.
 		빈 스캔을 할 것이 아니기 때문에, 자동등록되는 클래스라 하더라도 @Component를 붙여 줄 필요는 없다.
 		빈의 이름을 지정할 필요가 있다면 @Component("beanname")이나 @Named("beanname") 사용.
 	*/
+	
+	/**
+	 *   ※ 필드에 @Autowired가 붙어 있음
+	 *   - 필드나 프로퍼티 타입으로 di를 해주는데, 현재 등록된 Bean중에서 BeanB Type에 대입이 가능한 빈을 찾는다.
+	 *   - AnnotationConfigApplicationContext에서 BeanB를 등록하였기 때문에, BeanB가 @Autowired에 의해 di됨.
+	 *  
+	 *
+	 */
 	private static class BeanA{ @Autowired BeanB beanB; }	
 	private static class BeanB{}
 	
