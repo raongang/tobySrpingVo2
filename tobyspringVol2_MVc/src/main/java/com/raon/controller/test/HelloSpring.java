af6fc919e66c128f6e8817ccdded1c119490453d
raongang <sayllclubs@naver.com>
2020-04-03 11:51:46 +0900

    2020-04-03 raongang applicationContext Test

--- tobyspringVol2_MVc/src/main/java/com/raon/controller/test/HelloSpring.java
+++ tobyspringVol2_MVc/src/main/java/com/raon/controller/test/HelloSpring.java
@@ -2,6 +2,7 @@ package com.raon.controller.test;
 
 import org.springframework.stereotype.Component;
 
+/* applicationContext create Test */
 @Component
 public class HelloSpring {
 	public String sayHello(String name) {
