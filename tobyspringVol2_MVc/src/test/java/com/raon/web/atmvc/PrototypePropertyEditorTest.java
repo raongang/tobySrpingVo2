9972dbeb8b168cff1881f6720c3e66d6b8fa4e09
raongang <sayllclubs@naver.com>
2020-04-28 10:17:18 +0900

    2020-04-28 raongang

--- tobyspringVol2_MVc/src/test/java/com/raon/web/atmvc/PrototypePropertyEditorTest.java
+++ tobyspringVol2_MVc/src/test/java/com/raon/web/atmvc/PrototypePropertyEditorTest.java
@@ -7,9 +7,13 @@ import static org.junit.Assert.fail;
 import java.beans.PropertyEditorSupport;
 import java.io.IOException;
 
+import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.servlet.ServletException;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -19,10 +23,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.raon.web.AbstractDispatcherServletTest;
 
 /*
- *  
  *  @InitBinder나 WebBindingInitializer는 매번 new를 이용한다. 이를 해결하기 위해 싱글톤으로 할려고 했으나, prototypeEditor는 싱글톤 불가
  *  다른 빈의 di를 받기 위해서는 자기자신도 bean이 되어야 하지만 싱글톤이 되므로, prototype으로 사용한다.
- *  
  *  prototypeEditor 가 상태값을 가지기 때문에 멀티스레드 환경에서 여러 오브젝트가 공유해서 사용하면 안된다.
  * 
  * */
@@ -129,12 +131,64 @@ public class PrototypePropertyEditorTest extends AbstractDispatcherServletTest{
 	 *  프로토 타입 스코프 - 컨테이너에게 빈을 요청할 때마다 매번 새로운 오브젝트를 생성.
 	 */
 	
-	//프로토타입 도메인 오브젝트 프로퍼티 에디터 
+	// 프로토타입 도메인 오브젝트 프로퍼티 에디터 
 	//  - db에서 읽어온 완전한 Code오브젝트로 변환
 	//  - 다른 계층에서 사용시 제한을 받지 않음.
+	//  - 다른 bean을 DI받는 오브젝트는 자신도 빈이어야 한다.
+	
 	@Test
 	public void prototypePropertyEditor() throws ServletException, IOException{
+		setClasses(UserController2.class,CodePropertyEditor.class,CodeService.class);
 		
+		initRequest("/add.do").addParameter("id", "1").addParameter("name", "Spring").addParameter("userType", "2");
+		runService();
+		User user = (User)getModelAndView().getModel().get("user");
+		user.getUserType().getName();
 	}
 	
+	@Controller
+	static class UserController2{
+		@Inject Provider<CodePropertyEditor> codeEditorProvider;
+		
+		@InitBinder 
+		public void initBinder(WebDataBinder dataBinder) {
+			//get() - Provider를 이용해 프로토타입 빈의 새 오브젝트를 가져온다. @Scope
+			dataBinder.registerCustomEditor(Code.class,codeEditorProvider.get());
+		}
+		
+		/**
+		 * 리턴타입
+		 * 	 	1) @ModelAttribute 모델 오브젝트 또는 커맨드 오브젝트            
+	    		- 자동으로 컨트롤러가 리턴하는 모델에 추가됨.
+	    		- 기본적으로 모델 오브젝트 이름은 파라미터 타입이름.
+		 */
+		@RequestMapping("/add")
+		public void add(@ModelAttribute User user) {
+			System.out.println(user);
+		}
+	}
+	
+	//CodePropertyEditor는 bean으로 등록될것이기 때문에 @Autowired로 다른 bean 참조 가능
+	//@Component
+	@Scope("prototype")
+	static class CodePropertyEditor extends PropertyEditorSupport{
+		@Autowired CodeService codeService; 
+		
+		public void setAsText(String text) throws IllegalArgumentException{
+			setValue(this.codeService.getCode(Integer.parseInt(text)));
+		}
+		
+		public String getAsText() {
+			return String.valueOf(((Code)getValue()).getId()); 
+		}
+	}
+	
+	static class CodeService {
+		public Code getCode(int id) {
+			Code c = new Code();
+			c.setId(id);
+			c.setName("name");
+			return c;
+		}
+	}
 }
