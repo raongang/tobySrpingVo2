1d928fa616bceca0a8669e236e36f730097a4dce
raongang <sayllclubs@naver.com>
2020-05-11 17:38:32 +0900

    2020-05-11 raongang formatter add

--- tobyspringVol2_MVc/src/test/java/com/raon/web/atmvc/FormatterTest.java
+++ tobyspringVol2_MVc/src/test/java/com/raon/web/atmvc/FormatterTest.java
@@ -1,7 +1,17 @@
 package com.raon.web.atmvc;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.junit.Test;
+
 import com.raon.web.AbstractDispatcherServletTest;
 
 public class FormatterTest extends AbstractDispatcherServletTest{
 	
+	@Test
+	public void numberFormat() throws ServletException,IOException{
+		
+	}
 }
